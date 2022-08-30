58e46863060412303acd6e7b5e9d434513bb60d9
Pavel Popkov <goil2011@yandex.ru>
2022-08-30 11:23:49 +0300

    task68

---
+++ task68/Program.cs
@@ -0,0 +1,18 @@
+﻿int f(int m, int n)
+{
+    
+  if (m == 0) 
+    return n + 1;
+
+  else if (n == 0) 
+    return f(m - 1, 1);
+
+  else
+    return f(m - 1, f(m, n - 1));
+}
+Console.Write("Введи значение m : ");
+int m = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи значение n : ");
+int n = Convert.ToInt16(Console.ReadLine());
+
+Console.Write(f(m, n));
