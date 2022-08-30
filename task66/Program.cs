bd21caac420e906b2fbc71dc5f769e9537dec4f3
Pavel Popkov <goil2011@yandex.ru>
2022-08-30 11:22:21 +0300

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,12 @@
+﻿int f(int n, int m)
+{
+    if (m == 0) 
+        return 0;
+    return f(n, m-1) + m;
+}
+Console.Write("Введи значение от: ");
+int n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи значение до: ");
+int m = Convert.ToInt16(Console.ReadLine());
+
+Console.Write("Cумму натуральных элементов: " + f(n, m));
